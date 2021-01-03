a36f94bffcb36c1b6009cb49607789d04e5ee825
J-Z0807 <s17113209@stu.edu.tw>
2021-01-03 17:33:19 +0800

    Vue 電商開發

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  created() {
+    const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+</style>
